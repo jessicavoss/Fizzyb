b7b390e9a6f1f3030b06f6a22e59a3d6fe528b43
Jessica Voss <jessicavoss@mail.usf.edu>
2019-01-16 09:47:54 -0500

    Created initial Django app

--- Fizzyb/Fizzyb/urls.py
+++ Fizzyb/Fizzyb/urls.py
@@ -3,12 +3,15 @@ Definition of urls for Fizzyb.
 """
 
 from django.conf.urls import include, url
-
+import Counter.views
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$', Counter.views.index, name='index'),
+    url(r'^home$', Counter.views.index, name='home'),
+    ]
     # Examples:
     # url(r'^$', Fizzyb.views.home, name='home'),
     # url(r'^Fizzyb/', include('Fizzyb.Fizzyb.urls')),
@@ -18,4 +21,4 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-]
+
